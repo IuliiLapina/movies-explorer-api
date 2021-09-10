9f51247766e0b2ba8bde06ee1eb0a5b2ffeee45c
IuliiLapina <73750555+IuliiLapina@users.noreply.github.com>
2021-09-10 18:49:34 +0300

    add rate limit

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const router = require('./routes/index');
 const { requestLogger } = require('./middlewares/logger');
 const { errorLogger } = require('./middlewares/logger');
 const { errorHandler } = require('./middlewares/error-handler');
+const { limiter } = require('./middlewares/rate-limit');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -20,11 +21,10 @@ mongoose.connect('mongodb://localhost:27017/moviesdb', {
 });
 
 app.use(requestLogger); // логгер запросов
-// app.use(limiter);
+app.use(limiter);
 app.use(helmet());
 app.use(express.json());
 app.use(router);
-
 app.use(errorLogger); // логгер ошибок
 app.use(errors()); // обработчик ошибок celebrate
 app.use(errorHandler); // централизованный обработчик ошибок
