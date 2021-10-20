f101b8dc07aef444370fa93283f8de3f3256855a
Юлия Лапина <ul.lapina.97@gmail.com>
2021-10-21 00:05:33 +0300

    getMovies update

--- controllers/movies.js
+++ controllers/movies.js
@@ -5,7 +5,7 @@ const { NotFoundError } = require('../errors/not-found-error');
 const { ForbiddenError } = require('../errors/forbidden-error');
 
 module.exports.getMovies = (req, res, next) => {
-  Movie.find({})
+ Movie.find({ owner: req.user._id })
     .then((movie) => res.send(movie.reverse()))
     .catch(next);
 };
